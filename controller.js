d70a90a7ee5b240deac536d29456835ae6145ee3
adil ali <adiln1993@gmail.com>
2016-04-18 15:23:22 +0100

    added expressions to controller

--- controller.js
+++ controller.js
@@ -2,4 +2,11 @@ toDoApp.controller('ToDoController', function(){
   var toDo = this;
   toDo.todos = [{text: 'Todo 1', completed: true},
                 {text: 'Todo 2', completed: false}];
+  toDo.addToDo = function(todotext){
+    toDo.todos.push({text: todotext, completed: false});
+  };
+
+  toDo.removeToDo = function(){
+    toDo.todos.pop();
+  };
 });
